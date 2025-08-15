40d870358ec3ae012dbb05021015d95620d10047
fxi <f@fxi.io>
2025-08-15 16:20:50 +0200

    feat: pwa

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/nanote/dev-sw.js?dev-sw', { scope: '/nanote/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,56 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
+import { VitePWA } from "vite-plugin-pwa"; 
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+    manifest: {
+      name: "Nanote",
+      short_name: "Nanote",
+      description: "Distraction free text based notes",
+      theme_color: "#1c1917",
+      start_url: "/nanote/",
+      scope: "/nanote/",
+      display: "standalone",
+      background_color: "#1c1917",
+      "icons": [
+        {
+          "src": "pwa-64x64.png",
+          "sizes": "64x64",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "pwa-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "maskable-icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png",
+          "purpose": "maskable"
+        }
+      ]
+    },
+    devOptions: {
+      enabled: true,
+    },
+  }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  // Use relative paths for Tauri desktop builds, absolute path for web deployment
   base: process.env.BUILD_TARGET === 'desktop' ? './' : '/nanote/',
   optimizeDeps: {
     exclude: ['lucide-react'],
